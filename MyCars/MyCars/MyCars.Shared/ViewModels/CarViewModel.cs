d749d78fdb2e28ec55a32aa75f7f32a9ae0d8eae
delyan-nikolov-1992 <delyan.nikolov.1992@gmail.com>
2014-11-22 21:57:48 +0200

    Init Projects

---
+++ MyCars/MyCars/MyCars.Shared/ViewModels/CarViewModel.cs
@@ -0,0 +1,213 @@
+﻿namespace MyCars.ViewModels
+{
+    using GalaSoft.MvvmLight;
+    using MyCars.Models;
+    using Parse;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq.Expressions;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Windows.UI.Xaml.Media.Imaging;
+
+    public class CarViewModel : ViewModelBase
+    {
+        private string vendor;
+        private string model;
+        private double price;
+        private string imageUrl;
+        private string fullName;
+        private BitmapImage bitmap;
+        private string description;
+        private int yearOfManufacture;
+        private string cityLocation;
+        private string ownerUsername;
+        private string ownerPhone;
+
+        public static Expression<Func<Car, CarViewModel>> FromModel
+        {
+            get
+            {
+                return model => new CarViewModel()
+                {
+                    Vendor = model.Vendor,
+                    Model = model.Model,
+                    Description = model.Description,
+                    OwnerUsername = model.OwnerUsername,
+                    OwnerPhone = model.OwnerPhone,
+                    YearOfManufacture = model.YearOfManufacture,
+                    CityLocation = model.CityLocation,
+                    Price = model.Price,
+                    ImageUrl = model.ImageUrl,
+                    FullName = model.Vendor + " " + model.Model,
+                    Bitmap = new BitmapImage(new Uri(model.ImageUrl, UriKind.Absolute))
+                };
+            }
+        }
+
+        public Car ToModel
+        {
+            get
+            {
+                return new Car()
+                {
+                    Vendor = this.Vendor,
+                    Model = this.Model,
+                    Description = this.Description,
+                    OwnerUsername = ParseUser.CurrentUser.Username,
+                    OwnerPhone = ParseUser.CurrentUser["phone"].ToString(),
+                    YearOfManufacture = this.YearOfManufacture,
+                    CityLocation = this.CityLocation,
+                    Price = this.Price,
+                    ImageUrl = this.ImageUrl
+                };
+            }
+        }
+
+        public BitmapImage Bitmap
+        {
+            get
+            {
+                return this.bitmap;
+            }
+
+            set
+            {
+                this.bitmap = value;
+                this.RaisePropertyChanged(() => this.Bitmap);
+            }
+        }
+
+        public string Vendor
+        {
+            get
+            {
+                return this.vendor;
+            }
+            set
+            {
+                this.vendor = value;
+                this.RaisePropertyChanged(() => this.Vendor);
+            }
+        }
+
+        public string Model
+        {
+            get
+            {
+                return this.model;
+            }
+            set
+            {
+                this.model = value;
+                this.RaisePropertyChanged(() => this.Model);
+            }
+        }
+
+        public string FullName
+        {
+            get
+            {
+                return this.fullName;
+            }
+            set
+            {
+                this.fullName = value;
+                this.RaisePropertyChanged(() => this.FullName);
+            }
+        }
+
+        public string ImageUrl
+        {
+            get
+            {
+                return this.imageUrl;
+            }
+            set
+            {
+                this.imageUrl = value;
+                this.RaisePropertyChanged(() => this.ImageUrl);
+            }
+        }
+
+        public string Description
+        {
+            get
+            {
+                return this.description;
+            }
+            set
+            {
+                this.description = value;
+                this.RaisePropertyChanged(() => this.Description);
+            }
+        }
+
+        public string OwnerUsername
+        {
+            get
+            {
+                return this.ownerUsername;
+            }
+            set
+            {
+                this.ownerUsername = value;
+                this.RaisePropertyChanged(() => this.OwnerUsername);
+            }
+        }
+
+        public string OwnerPhone
+        {
+            get
+            {
+                return this.ownerPhone;
+            }
+            set
+            {
+                this.ownerPhone = value;
+                this.RaisePropertyChanged(() => this.OwnerPhone);
+            }
+        }
+
+       
+
+        public int YearOfManufacture
+        {
+            get
+            {
+                return this.yearOfManufacture;
+            }
+            set
+            {
+                this.yearOfManufacture = value;
+                this.RaisePropertyChanged(() => this.YearOfManufacture);
+            }
+        }
+
+        public string CityLocation
+        {
+            get
+            {
+                return this.cityLocation;
+            }
+            set
+            {
+                this.cityLocation = value;
+                this.RaisePropertyChanged(() => this.CityLocation);
+            }
+        }
+
+        public double Price
+        {
+            get
+            {
+                return this.price;
+            }
+            set
+            {
+                this.price = value;
+                this.RaisePropertyChanged(() => this.Price);
+            }
+        }
+    }
+}
---
+++ MyCars/MyCars/MyCars.WindowsPhone/Pages/Main/MainPageViewModel.cs
@@ -0,0 +1,87 @@
+﻿namespace MyCars.Pages.Main
+{
+    using GalaSoft.MvvmLight;
+    using MyCars.Models;
+    using MyCars.ViewModels;
+    using Parse;
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.ObjectModel;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    public class MainPageViewModel : ViewModelBase
+    {
+        private const string DefaultUrl = 
+            "http://nancyharmonjenkins.com/wp-content/plugins/nertworks-all-in-one-social-share-tools/images/no_image.png";
+
+        private ObservableCollection<CarViewModel> cars;
+        private bool initializing;
+
+        public MainPageViewModel()
+        {
+            this.LoadCars();
+        }
+
+        private async void LoadCars()
+        {
+            this.Initializing = true;
+
+            var cars = await new ParseQuery<Car>()
+                    .FindAsync();
+
+            this.Cars = cars.AsQueryable()
+                    .Select(CarViewModel.FromModel);
+
+            this.Initializing = false;
+        }
+
+        public bool Initializing
+        {
+            get
+            {
+                return this.initializing;
+            }
+            set
+            {
+                this.initializing = value;
+                this.RaisePropertyChanged(() => this.Initializing);
+            }
+        }
+
+        public IEnumerable<CarViewModel> Cars
+        {
+            get
+            {
+                if (this.cars == null)
+                {
+                    this.cars = new ObservableCollection<CarViewModel>();
+                }
+
+                return this.cars;
+            }
+            set
+            {
+                if (this.cars == null)
+                {
+                    this.cars = new ObservableCollection<CarViewModel>();
+                }
+
+                this.cars.Clear();
+
+                foreach (var currentCar in value)
+                {
+                    try
+                    {
+                        this.cars.Add(currentCar);
+                    }
+                    catch (Exception)
+                    {
+                        currentCar.ImageUrl = DefaultUrl;
+                        this.cars.Add(currentCar);
+                    }
+                }
+            }
+        }
+    }
+}
